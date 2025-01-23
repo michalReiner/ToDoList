8c8c163d8949ede6c6e9a81f9d6e80706ef1c7c4
michalReiner <michal3213031@gmail.com>
2025-01-23 12:48:45 +0200

    Initial commit

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d32c7a84f07915063e0badcfacaa78499e7207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4383e568383f6ea9cbe9818ea95f54616655a51d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
